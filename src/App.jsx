3a6262d5505c54fba4029dfe9dfcdb8c1c619078
Nahada C <nahadac701@gmail.com>
2025-05-19 21:28:19 +0530

    Re design

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <div className="row m-0  w-100 " style={{minHeight:"90vh"}}>
         <div className="col-md-6 m-0 bg-light text-dark d-flex align-items-center justify-content-center p-5">
           <div>
-            <h1 className='main_head'>Your trusted <br /> partner in <br /> manpower , <br /> materials supply & Scrap Purchase</h1>
+            <h1 className='main_head'>Your trusted <br /> partner in <br /> manpower , <br /> material supply & Scrap Purchase</h1>
             <h6>Deliveing workforce  and materials  <br /> to empower to your projects</h6>
             <a href='#contact' className='my-4 btn btn-blue-yellow'>
               Get in Touch
